804b87e54d720bc6f108e34554393b645627b8a3
Louis Afful <louisafful1@gmail.com>
2025-08-02 05:01:55 +0000

    Set up Express backend with classify route to connect with Python ML API

---
+++ backend/routes/classify.js
@@ -0,0 +1,19 @@
+// classify.js
+import express from 'express';
+import axios from 'axios';
+
+const classifyRoute = express.Router();
+
+classifyRoute.post('/', async (req, res) => {
+  const { comment } = req.body;
+
+  try {
+    const response = await axios.post('http://127.0.0.1:5000/predict', { comment });
+    res.json(response.data);  // { prediction: 0 or 1 }
+  } catch (error) {
+    console.error('Error calling Python API:', error.message);
+    res.status(500).json({ error: 'Could not get prediction from model' });
+  }
+});
+
+export default classifyRoute;
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import classifyRoute from './routes/classify.js';
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+
+const app = express();
+
+
+ //Cross-Origin Resource Sharing
+ app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }));
+  
+
+ app.use(express.json());
+ app.use(express.urlencoded({extended: true})); 
+
+
+app.use('/api/classify', classifyRoute);
+
+app.use(notFound);
+app.use(errorHandler);
+
+server.listen(port, () => console.log(`Server started on port ${port}`));
+
